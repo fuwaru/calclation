a7829b02e77e8b610270cff66db5e70a6f52e545
fuwari <black.waffle334@gmail.com>
2023-09-26 10:07:19 +0900

    [Update] 辞書の追加

--- calclation.py
+++ calclation.py
@@ -2,6 +2,12 @@
 print("EasyCalc v.1.0.0")
 
 #変数
+dict_command ={}
+dict.update({"command":"!end !calclate !canI"
+            ,"end":"プログラムを終了します"
+            ,"calclate":"+：和\n-：差\n/：商\n*：積\n%：余り\n"
+            ,"canI":"和差商積と余りの計算ができます"})
+
 flg_End = False
 
 #初期表示文字
@@ -10,22 +16,34 @@ print("また、半角でかつ空白文字を入力しないでください")
 print("\n !commandでコマンド一覧を閲覧できます。\nこの計算機のできることや四則演算その他計算のやり方を見ることができます")
 
 
-def calclator(text_calc):
+
+#テキストの種類
+def reading_function(text_calc):
     if(text_calc == "!end"):
         return 1
     elif(text_calc == "123"):
         return 0
     else:
         print("不正な文字列です")
+        return 2
     
+
+#命令:コマンド
+def command():
+    print("coming soon...")
+
+#命令:計算処理
+def calclator():
+    print("coming soon...")
 
 #処理本文
 while True:
     text_calc = input(">>> ")
     
     # "!"が最初についているテキストの判別
-    test = calclator(text_calc)
+    test = reading_function(text_calc)
     print(test)
     break
+
 
 
